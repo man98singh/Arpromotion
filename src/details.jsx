f39f9a6be66905dfc3601f86571b6abf05bb5ff2
Manish <man98singh@gmail.com>
2024-10-11 21:24:43 +0530

    final version/// step 2 css canvas

--- src/details.jsx
+++ src/details.jsx
@@ -86,7 +86,7 @@ const Details = ({ capturedImage, onShare, onReset }) => {
 στην κλήρωση
 & αποστολή
 της φωτογραφίας</h2>
-<img src='/buttons/popupbuttonAsset 7.png'/>
+<img src='/buttons/1.png'/>
             <form onSubmit={(e) => e.preventDefault()}>
                 <div>
                     <h6 className='sub-header'>Πεδία υποχρεωτικής συμπλήρωσης:</h6>
