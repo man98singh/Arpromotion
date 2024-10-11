a971acce05836e8a89cc22dbd7e13cb211b8d3cf
Manish <man98singh@gmail.com>
2024-10-11 13:39:49 +0530

    final version/// step 1 css canvas

--- src/CaptureCamera.jsx
+++ src/CaptureCamera.jsx
@@ -4,15 +4,15 @@ const CaptureControls = ({ onCapture}) => {
     return (
         <div style={{ 
             position: 'absolute', 
-            bottom: '-18px', 
+            bottom: '252px', 
             left: '50%', 
             transform: 'translateX(-50%)', 
             display: 'flex', 
             gap: '10px', 
             zIndex: 1 
-        }}>
+        }} className='capture-button-container'>
             <img 
-                src="/buttons/fcameraAsset 12.png"  
+                src="/buttons/fcameraAsset 12@2x.png"  
                 alt="Capture"
                 className="capture-button"
                 onClick={onCapture}
