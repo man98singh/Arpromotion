dfa17293ed49939bc6f8bccccb1580486004c111
Manish <man98singh@gmail.com>
2024-10-11 14:09:52 +0530

    final version/// step 1 css canvas

--- src/CaptureCamera.jsx
+++ src/CaptureCamera.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-
+import "./LiveCamera.css"
 const CaptureControls = ({ onCapture}) => {
     return (
-        <div style={{ 
-            position: 'absolute', 
-            bottom: '252px', 
-            left: '50%', 
-            transform: 'translateX(-50%)', 
-            display: 'flex', 
-            gap: '10px', 
-            zIndex: 1 
-        }} className='capture-button-container'>
+        <div className='capture-button-container'>
             <img 
                 src="/buttons/fcameraAsset 12@2x.png"  
                 alt="Capture"
