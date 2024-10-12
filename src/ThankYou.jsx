a1d26cb1dba05f6e2a0af90831524a03c57e001f
Manish <man98singh@gmail.com>
2024-10-12 14:10:12 +0530

    final version/// step 2 css canvas

--- src/ThankYou.jsx
+++ src/ThankYou.jsx
@@ -4,8 +4,9 @@ const ThankYou = ({ onReset }) => {
     return (
         <div className="thankyou-container">
             <h1>Thank you!</h1>
-           
+           <div className='restart-name'>
             <img src='/buttons/frestartAsset 15@3x.png' onClick={onReset} alt="restart" className='restart-button'></img>
+        </div>
         </div>
     );
 };
